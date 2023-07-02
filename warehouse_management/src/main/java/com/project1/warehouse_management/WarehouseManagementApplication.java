bc22ea0db87a6e30bb59c5e5fd13e46d9d88d708
jarsenau11 <jarsenau@asu.edu>
2023-07-02 11:55:06 -0700

    project setup

---
+++ warehouse_management/src/main/java/com/project1/warehouse_management/WarehouseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.project1.warehouse_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseManagementApplication.class, args);
+	}
+
+}
